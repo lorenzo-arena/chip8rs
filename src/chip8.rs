22480553302f296d37db0cd0a3789f41c35bc190
Lorenzo Arena <arena.lor@gmail.com>
2021-09-25 23:38:58 +0200

    chip8: add first basic fetch/execute loop for IBM demo

--- src/chip8.rs
+++ src/chip8.rs
@@ -2,14 +2,21 @@
 use crate::display::Display;
 use crate::display::NCursesDisplay;
 use crate::fonts::Fonts;
+use crate::logger::Logger;
+use crate::logger::FileLogger;
 
 use std::{thread, time, fs};
 
 const MEMORY_SIZE: usize = 4096;
 const STACK_SIZE: usize = 100;
 const REGISTERS_SIZE: usize = 16;
-const FONT_START: usize = 0x50;
-const ROM_START: usize = 0x200;
+const FONT_START: u16 = 0x50;
+const ROM_START: u16 = 0x200;
+
+const DISPLAY_WIDTH: usize = 64;
+const DISPLAY_HEIGHT: usize = 32;
+
+const LOG_FILE: &str = "chip8rs.log";
 
 /* TODO : restore debug trait */
 /* TODO : use arrays instead of vecs? */
@@ -22,13 +29,14 @@ pub struct Chip8 {
     delay_timer: u8,
     sound_timer: u8,
     regs: [u8; REGISTERS_SIZE],
-    fonts: Fonts
+    fonts: Fonts,
+    logger: FileLogger,
 }
 
 impl Chip8 {
     pub fn new() -> Chip8 {
         Chip8 {
-            display: NCursesDisplay::new(64, 32),
+            display: NCursesDisplay::new(DISPLAY_WIDTH, DISPLAY_HEIGHT, false),
             memory: [0; MEMORY_SIZE],
             pc: 0,
             i: 0,
@@ -36,12 +44,13 @@ impl Chip8 {
             delay_timer: 0,
             sound_timer: 0,
             regs: [0; REGISTERS_SIZE],
-            fonts: Fonts::new()
+            fonts: Fonts::new(),
+            logger: FileLogger::new(LOG_FILE.to_string()),
         }
     }
 
     fn load_fonts(& mut self) {
-        let mut dest = FONT_START;
+        let mut dest = FONT_START as usize;
 
         for font in self.fonts.fonts {
             self.memory[dest..(dest + font.len())].copy_from_slice(&font);
@@ -52,39 +61,128 @@ impl Chip8 {
     fn load_rom(& mut self, rom_path: &str) {
         let file_content = fs::read(rom_path).unwrap();
 
-        if file_content.len() > (MEMORY_SIZE - ROM_START) {
+        if file_content.len() > (MEMORY_SIZE - ROM_START as usize) {
             panic!("ROM content is too large");
         }
 
-        let dest = ROM_START;
+        let dest = ROM_START as usize;
         self.memory[dest..(dest + file_content.len())].copy_from_slice(&file_content);
+    }
+
+    fn fetch(& mut self) -> u16 {
+        let first = self.memory[self.pc as usize] as u16;
+        let second = self.memory[(self.pc + 1) as usize] as u16;
+
+        self.pc += 2;
+
+        (first << 8) | second
+    }
+
+    fn execute(& mut self, instr: u16) {
+        if (instr & 0xF000) == 0x0000 {
+            if instr == 0x00E0 {
+                /* 00E0: clear screen instruction, turn all pixels off */
+                self.display.clear_screen(false);
+                self.display.refresh();
+            } else if instr == 0x00EE {
+                /* 00EE: return from subroutine, pop the PC */
+                self.pc = self.stack.pop().unwrap();
+            }
+        } else if (instr & 0xF000) == 0x1000 {
+            /* 1NNN: jump, set the PC to NNN */
+            self.pc = instr & 0x0FFF;
+        } else if (instr & 0xF000) == 0x2000 {
+            /* 2NNN: call subroutine, push the PC and set the PC to NNN */
+            self.stack.push(self.pc);
+            self.pc = instr & 0x0FFF;
+        } else if (instr & 0xF000) == 0x6000 {
+            /* 6XNN: set register X to value NN */
+            let reg = (instr & 0x0F00) >> 8;
+            self.regs[reg as usize] = (instr & 0x00FF) as u8;
+        } else if (instr & 0xF000) == 0x7000 {
+            /* 7XNN: add value to register X; this can overflow, so a helper variable is used */
+            let reg = (instr & 0x0F00) >> 8;
+            let mut curr_reg = self.regs[reg as usize] as u16;
+            curr_reg += (instr & 0x00FF) as u16;
+            self.regs[reg as usize] = (curr_reg & 0x00FF) as u8;
+        } else if (instr & 0xF000) == 0xA000 {
+            /* ANNN: set index to value NNN */
+            self.i = instr & 0x0FFF;
+        } else if (instr & 0xF000) == 0xD000 {
+            /* DXYN: display */
+            self.draw_sprite(instr & 0x0FFF);
+        }
+    }
+
+    /* TODO : this should be moved to another entity */
+    fn draw_sprite(& mut self, instr: u16) {
+        /* Get X and Y coordinates from the registers */
+        let x = (instr & 0x0F00) >> 8;
+        let x = self.regs[x as usize] % (DISPLAY_WIDTH as u8);
+        let y = (instr & 0x00F0) >> 4;
+        let y = self.regs[y as usize] % (DISPLAY_HEIGHT as u8);
+
+        /* Set VF to 0 as default; it will be set to 1 if any pixel is turned off */
+        self.regs[0x0F as usize] = 0;
+
+        let n = (instr & 0x000F) as u8;
+
+        self.logger.log(format!("Drawing sprite from coordinates {}:{}, size {}", x, y, n));
+
+        for sprite_row in 0..n {
+            let y_pos = (y + sprite_row) as usize;
+            if y_pos < DISPLAY_HEIGHT {
+                let sprite_data = self.memory[(self.i + (sprite_row as u16)) as usize];
+                self.logger.log(format!("Sprite data: {:X?}", sprite_data));
+
+                for sprite_bit_i in 0..8 {
+                    let x_pos = (x + sprite_bit_i) as usize;
+                    if x_pos < DISPLAY_WIDTH {
+                        /* The bits must be read from MIB to LIB */
+                        let bit_index = 7 - sprite_bit_i;
+                        let bit_value = (sprite_data & (0x1 << bit_index)) >> bit_index;
+                        let led_status = self.display.is_on(x_pos, y_pos);
+                        
+                        /* If current pixel is on and bit is high, flip the led */
+                        if (bit_value != 0) && led_status {
+                            self.display.led_off(x_pos, y_pos);
+                            self.display.refresh();
+                            
+                            /* Set VF to 1 since a led has been changed */
+                            self.regs[0x0F as usize] = 1;
+                        } else if (bit_value != 0) && !led_status {
+                            self.logger.log(format!("Turning ON led {}:{}", x_pos, y_pos));
+                            self.display.led_on(x_pos, y_pos);
+                            self.display.refresh();
+                        }
+                    } else {
+                        self.logger.log(format!("X overflow while drawing sprite"));
+                    }
+                }
+            } else {
+                self.logger.log(format!("Y overflow while drawing sprite"));
+            }
+        }
     }
 
     pub fn run(& mut self, rom_path: &str) {
         self.load_fonts();
         self.load_rom(rom_path);
 
-        /* TODO: Used to debug memory content, to remove */
-        //println!("{:x?}", self.memory);
-        //loop {}
+        self.pc = ROM_START;
 
         self.display.open();
 
-        let mut y = 0;
+        loop {
+            let instr = self.fetch();
+            self.execute(instr);
 
-        while y < 32 {
-            let mut x = 0;
-            while x < 64 {
-                self.display.led_off(x, y);
-                self.display.refresh();
-
-                let millis = time::Duration::from_millis(16);
-                thread::sleep(millis);
-
-                x += 1;
-            }
-
-            y += 1;
+            /* TODO : timing can be implemented better; but supposing that the fetch/execution times
+             * are negligible, a 2 ms sleep will make the emulator execute ~500 instruction per seconds.
+             * It seems like a standard speed of 700 CHIP-8 instructions per seconds fits well enough for
+             * most games. */
+            let millis = time::Duration::from_millis(2);
+            thread::sleep(millis);
         }
 
         self.display.close();
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 mod chip8;
 mod display;
 mod fonts;
+mod logger;
 
 use chip8::Chip8;
 
 fn main() {
     let mut chip = Chip8::new();
-    let rom_path = "particle_demo.ch8";
+    let rom_path = "ibm_logo.ch8";
 
     chip.run(rom_path);
 }
