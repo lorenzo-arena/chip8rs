ba2cd20699417fea631f2e0c3ec48fdae995d57a
Lorenzo Arena <arena.lor@gmail.com>
2021-09-26 18:43:12 +0200

    main: add crate to parse command line options

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,23 @@ mod fonts;
 mod logger;
 
 use chip8::Chip8;
+use clap::{Arg, App};
 
 fn main() {
-    let mut chip = Chip8::new();
-    let rom_path = "test_opcode.ch8";
+    let matches = App::new("chip8rs")
+                          .version("0.0.1")
+                          .author("Lorenzo A.")
+                          .about("CHIP-8 emulator written in Rust")
+                          .arg(Arg::with_name("rom")
+                               .short("r")
+                               .long("rom")
+                               .value_name("FILE")
+                               .help("Path to the CHIP-8 ROM file")
+                               .required(true)
+                               .takes_value(true))
+                          .get_matches();
 
+    let rom_path = matches.value_of("rom").unwrap();
+    let mut chip = Chip8::new();
     chip.run(rom_path);
 }
